285b4414c54c598eb5c764fad1dbf641832ca69f
Kai Wells <me@quells.net>
2013-02-06 11:25:10 -0600

    Changed from bash curl to JSON module

--- darksky.py
+++ darksky.py
@@ -1,28 +1,22 @@
 from xml.etree import ElementTree as ET
-import string
-from subprocess import Popen, PIPE, STDOUT
-import re
+import urllib, urllib2
+import json
 
 def forecast(APIKEY, LAT, LONG):
-	bash = '"curl -s https://api.darkskyapp.com/v1/brief_forecast/' + APIKEY + '/' + LAT + ',' + LONG + '"'
-	forecast = str(Popen('bash -i -c ' + bash, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT).communicate()).split(',')
-	# REGEX
-	quot = re.compile("\"")
-	# HARVEST
-	temp = forecast[0].split(":")[2]
-	condition = quot.sub("", forecast[1].split(":")[1])
-	condition = condition.capitalize()
-	weather = temp + "F, " + condition
-	future = quot.sub("", forecast[3].split(":")[1]) + "."
-	future = future.capitalize()
+	# GET JSON DATA
+	url = 'https://api.darkskyapp.com/v1/brief_forecast/' + APIKEY + '/' + LAT + ',' + LONG
+	response = urllib2.urlopen(url)
+	response_items = response.read()
+	item = json.loads(response_items)
+	# PARSE ITEMS
 	xml = []
-	xml.append({
-		'title': weather,
-		'subtitle': future,
-		'arg': 'arg',
-		'uid': '123456789',
-		'icon': 'darksky.png'
-		})
+	xml.append ({
+		'title': str(item[u'currentTemp']) + 'F, ' + item[u'hourSummary'].capitalize(),
+		'subtitle': item[u'daySummary'].capitalize() + '.',
+		'arg': "'" + LAT + ',' + LONG + "'",
+		'uid': str(item[u'checkTimeout']),
+		'icon': 'icon.png'
+	})
 	# PLANT XML TREE
 	xml_items = ET.Element('items')
 	for item in xml:
