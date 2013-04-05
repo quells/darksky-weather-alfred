8befb84fc8d1b7bea1de08477e80d4c70ebee32d
Kai Wells <me@quells.net>
2013-04-04 21:14:59 -0500

    Forecast.io API

--- darksky.py
+++ darksky.py
@@ -4,17 +4,31 @@ import json
 
 def forecast(APIKEY, LAT, LONG):
 	# GET JSON DATA
-	url = 'https://api.darkskyapp.com/v1/brief_forecast/' + APIKEY + '/' + LAT + ',' + LONG
+	url = 'https://api.forecast.io/forecast/' + APIKEY + '/' + LAT + ',' + LONG
 	response = urllib2.urlopen(url)
 	response_items = response.read()
 	item = json.loads(response_items)
 	# PARSE ITEMS
 	xml = []
 	xml.append ({
-		'title': str(item[u'currentTemp']) + 'F, ' + item[u'hourSummary'].capitalize(),
-		'subtitle': item[u'daySummary'].capitalize() + '.',
-		'arg': "'" + LAT + ',' + LONG + "'",
-		'uid': str(item[u'checkTimeout']),
+		'title': str(int(item[u'currently'][u'temperature']*100)/100) + 'F, ' + item[u'minutely'][u'summary'].capitalize(),
+		'subtitle': item[u'hourly'][u'summary'].capitalize(),
+		'arg': LAT + ',' + LONG,
+		'uid': str(int(item[u'hourly'][u'data'][0][u'time'])),
+		'icon': 'icon.png'
+	})
+	xml.append ({
+		'title': 'Today: ' + str(int(item[u'daily'][u'data'][0][u'temperatureMin']*100)/100) + 'F - ' + str(int(item[u'daily'][u'data'][0][u'temperatureMax']*100)/100) + 'F',
+		'subtitle': item[u'daily'][u'data'][0][u'summary'].capitalize(),
+		'arg': LAT + ',' + LONG + ',' + str(int(item[u'daily'][u'data'][0][u'time'])),
+		'uid': str(int(item[u'daily'][u'data'][0][u'time'])),
+		'icon': 'icon.png'
+	})
+	xml.append ({
+		'title': 'Tomorrow: ' + str(int(item[u'daily'][u'data'][1][u'temperatureMin']*100)/100) + 'F - ' + str(int(item[u'daily'][u'data'][1][u'temperatureMax']*100)/100) + 'F',
+		'subtitle': item[u'daily'][u'data'][1][u'summary'].capitalize(),
+		'arg': LAT + ',' + LONG + ',' + str(int(item[u'daily'][u'data'][1][u'time'])),
+		'uid': str(int(item[u'daily'][u'data'][1][u'time'])),
 		'icon': 'icon.png'
 	})
 	# PLANT XML TREE
